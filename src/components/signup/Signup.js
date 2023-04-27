cc37ef6f07193955aab90a26281c7043ad30a0df
ZaxtuN <898602+ZaxtuN@users.noreply.github.com>
2023-04-27 09:36:39 -0400

    redoing my firebase

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,7 +1,36 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./signup.css";
+import { auth } from "../../firebaseConfig";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSignUp = async () => {
+    if (password == confirmPassword) {
+      try {
+        // const user = await createUserWithEmailAndPassword({
+        //   auth,
+        //   email,
+        //   password,
+        // });
+        // console.log(user);
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        setFullName("");
+      } catch (error) {
+        console.log(error);
+        console.log(error.response);
+      }
+    } else {
+      alert("passwords do not match");
+    }
+  };
+
   return (
     <div class="bg-grey-lighter min-h-screen flex flex-col">
       <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
@@ -10,8 +39,11 @@ const SignUp = () => {
           <input
             type="text"
             class="block border border-grey-light w-full p-3 rounded mb-4"
-            name="fullname"
+            name="fullName"
             placeholder="Full Name"
+            onChange={(e) => {
+              setFullName(e.target.value);
+            }}
           />
 
           <input
@@ -19,6 +51,9 @@ const SignUp = () => {
             class="block border border-grey-light w-full p-3 rounded mb-4"
             name="email"
             placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
 
           <input
@@ -26,20 +61,27 @@ const SignUp = () => {
             class="block border border-grey-light w-full p-3 rounded mb-4"
             name="password"
             placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <input
             type="password"
             class="block border border-grey-light w-full p-3 rounded mb-4"
-            name="confirm_password"
+            name="confirmPassword"
             placeholder="Confirm Password"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
           />
 
           <button
             type="submit"
             class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
             className="createAccountButton"
+            onClick={handleSignUp}
           >
-            Create Account
+            Create Account and Login
           </button>
         </div>
       </div>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOiPonjI1uZWrsIFwf2TjqJ-uU6hZXeXc",
+  authDomain: "takemetoyourpizza-fdd2d.firebaseapp.com",
+  projectId: "takemetoyourpizza-fdd2d",
+  storageBucket: "takemetoyourpizza-fdd2d.appspot.com",
+  messagingSenderId: "43763848565",
+  appId: "1:43763848565:web:8c883fae9c0285d034af3c",
+  measurementId: "G-XLH6NGW5ZX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
