ee63439061839f49b24d2b9dedb18251d44c17d4
ZaxtuN <898602+ZaxtuN@users.noreply.github.com>
2023-04-29 11:14:11 -0400

    Working on adding firestore capability

--- src/components/pizzashops/PizzaShops.js
+++ src/components/pizzashops/PizzaShops.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import PizzaSidebar from "../dashboard/PizzaSidebar/PizzaSidebar";
+import { getDocs, addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
+
 const PizzaShops = () => {
+  useEffect(() => {
+    let pizzashops = getDocs(collection(db, "PizzaShops"));
+    pizzashops.forEach((shop) => {
+      console.log(`${shop.avgRating}`);
+    });
+  }, []);
+
   return (
     <ProSidebarProvider backgroundColor="#FFA600">
       <PizzaSidebar />
-      <div className="ml-72">PizzaShops</div>;
+      <div className="ml-72">
+        <h1>yo test</h1>
+      </div>
     </ProSidebarProvider>
   );
 };
